786ef96d4db90f0c7b222890e4e825e272edf232
Vinicius <dev.viniciuspd@gmail.com>
2024-02-14 18:02:53 -0300

    First commit on Angular project to create profile page

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { LupumButton } from './directives/lupum-button.directive';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, LupumButton],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  constructor(private router: Router) {
+  }
+
+  onClick(event: any) {
+    this.router.navigate(['']);
+  }
+
+
+
+}
