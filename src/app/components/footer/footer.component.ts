bbe16707e8d0bf937fbb7d8151ec1a91b4b721cb
Vinicius Dionizio <dev.viniciuspd@gmail.com>
2026-07-30 19:29:38 -0300

    feat: enhance footer component to display version information and update layout

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,17 +2,22 @@
 import { Component } from '@angular/core';
 import { versionInfo } from '../../version-info';
 import { socials } from '../utils/socials';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
-    selector: 'app-footer',
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  imports: [DatePipe],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   readonly versionInfo = versionInfo;
   currentYear = new Date().getFullYear();
+
+  ngOnInit() {
+    console.log('Version Info:', this.versionInfo);
+  }
 
   socials = socials;
 
